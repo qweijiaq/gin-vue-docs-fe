611a9ef4ecf74afde79bcd2e2a6870f9549dfd3f
weijia <2112025700@qq.com>
2023-12-26 14:08:12 +0800

    feat: 后台页面框架搭建

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import ArcoVue from '@arco-design/web-vue'
 import '@arco-design/web-vue/dist/arco.css'
+import '@/assets/theme.css'
+import '@/assets/reset.css'
 
 import App from './App.vue'
 import router from './router'
--- src/views/admin/index.vue
+++ src/views/admin/index.vue
@@ -1,7 +1,72 @@
 <template>
-    <router-view />
+    <div class="admin">
+        <aside>
+            <div class="logo">LOGO</div>
+            <div class="menu"></div>
+        </aside>
+        <div class="main">
+            <header>
+                <div class="left"></div>
+                <div class="right"></div>
+            </header>
+            <div class="tabs"></div>
+            <main>
+                <router-view />
+            </main>
+        </div>
+    </div>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+    .admin {
+        display: flex;
+    }
+
+    aside {
+        background-color: var(--color-bg-1);
+        width: 240px;
+        border-right: 1px solid var(--bg);
+
+        > .logo {
+            width: 100%;
+            height: 90px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background-color: rgb(var(--arcoblue-2));
+        }
+
+        > .menu {
+            width: 100%;
+        }
+    }
+
+    .main {
+        overflow-y: auto;
+        width: calc(100vw - 240px);
+
+        > header {
+            height: 60px;
+            background-color: var(--color-bg-1);
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            padding: 0 20px;
+        }
+
+        > .tabs {
+            height: 30px;
+            background-color: var(--color-bg-1);
+            padding: 0 20px;
+            border-top: 1px solid var(--bg);
+        }
+
+        > main {
+            min-height: calc(100vh - 90px);
+            background-color: var(--bg);
+            padding: 20px;
+        }
+    }
+</style>
