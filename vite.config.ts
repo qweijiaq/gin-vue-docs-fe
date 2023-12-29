3edbdc92bb2f51aa297b36cd0ea89f1b3424fcdd
weijia <2112025700@qq.com>
2023-12-29 16:50:22 +0800

    feat: 前台页面 —— 配置环境变量

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,19 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig, loadEnv } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  console.log(env);
+  return defineConfig({
+    plugins: [vue()],
+    envDir: "./", // 配置文件相对于根目录的位置
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+  });
+});
